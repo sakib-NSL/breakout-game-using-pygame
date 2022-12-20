0ff056429dbd0e79b07fc42b21adbebe9ab7d637
sakib-NSL <sakib@nextsolutionlab.com>
2022-12-20 09:20:54 +0600

    first commit

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+class Config:
+    # colors
+    WHITE = (255,255,255)
+    DARKBLUE = (36,90,190)
+    LIGHTBLUE = (0,176,240)
+    RED = (255,0,0)
+    ORANGE = (255,100,0)
+    YELLOW = (255,255,0)
+    GREEN = (0, 255, 0)
+    CYAN = (0,255,255)
+
+    SIZE = (800, 600)
+
+    SCREEN_SCALER =(SIZE[0]/800, SIZE[1]/600) 
+    LIFE = 100
+
+    NO_OF_BRICKS = 35 # multiple of 7
+    PADDLE_SPEED = 5
+    FRAME_DEVISOR_FOR_BRICKS = 8
+
+    BACKGROUND_IMAGE_PATH = "static_files/background.jpg"
+    BULLET_IMAGE_PATH = "static_files/bullet.png"
